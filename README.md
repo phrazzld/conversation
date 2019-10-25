7c572afa4a7e95a33b6fbaf5eac542e244a70913
Phaedrus <phraznikov@gmail.com>
2019-10-25 14:27:04 -0700

    More thoughtful config messages, add (sparse) README

---
+++ README.md
@@ -0,0 +1,7 @@
+# RoboPeterson API
+
+Node API for interfacing with the Dialogflow chatbot, RoboPeterson. MongoDB for storing messages and users.
+
+## License
+
+MIT
--- gcpSetup.js
+++ gcpSetup.js
@@ -3,5 +3,9 @@ const fs = require('fs');
 console.log('process.env.GCP_KEY_FILE:', process.env.GCP_KEY_FILE);
 
 fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, err => {
-  console.log('Hit err:', err);
+  if (err) {
+    console.error('Hit err:', err);
+  } else {
+    console.log('Smooth sailing Captain');
+  }
 });
